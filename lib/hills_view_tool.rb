d751a83f49dacc2d985bd2e44c72cb4a6e10c260
SiamKing <thesiameseelectric@gmail.com>
2017-12-01 12:51:08 -0800

    Dev tool initial commit

---
+++ lib/hills_view_tool.rb
@@ -0,0 +1,6 @@
+require "hills_view_tool/version"
+require "hills_view_tool/renderer"
+
+module HillsViewTool
+  # Your code goes here...
+end
---
+++ lib/hills_view_tool/version.rb
@@ -0,0 +1,3 @@
+module HillsViewTool
+  VERSION = "0.1.0"
+end
